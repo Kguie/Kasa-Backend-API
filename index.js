287ff2c488efd16e74cd96dc7b06ae48db37f7ad
Kévin Guieba <kevin.guieba@gmail.com>
2022-11-10 23:20:52 -0400

    ajout de cors

--- index.js
+++ index.js
@@ -4,26 +4,15 @@ const mongoose = require('mongoose')
 const app = express()
 const Lodging = require('./models/lodging')
 const PORT = 4000
+const cors = require('cors')
 
 //Cors
-app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization");
-    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, PATCH, OPTIONS");
-    next();
-});
+app.use(cors())
 
 
 //Database connection
 
 app.get('/lodgings', async (req, res) => {
-    mongoose.connect("mongodb+srv://kguie:GfQLpqTpFOeXvAFr@kasa.nsgwlcc.mongodb.net/KasaDB", {
-        useNewUrlParser: true,
-        useUnifiedTopology: true
-    })
-        .then(() => console.log("Connexion à MongoDB réussie !"))
-        .catch(() => console.log("Connexion à MongoDB échouée !"));
-
 
     const results = await Lodging.find({})
 
