384b7057e6f0e2463d242b5287f44d93b6648322
Kévin Guieba <kevin.guieba@gmail.com>
2022-11-10 22:16:25 -0400

    test3

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
+// index.js
 const express = require('express')
-
+const mongoose = require('mongoose')
 const app = express()
+const Lodging = require('./models/lodging')
 const PORT = 4000
 
 app.listen(PORT, () => {
     console.log(`API listening on PORT ${PORT} `)
 })
+
+//Database connection
+mongoose.connect("mongodb+srv://kguie:GfQLpqTpFOeXvAFr@kasa.nsgwlcc.mongodb.net/KasaDB", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+    .then(() => console.log("Connexion à MongoDB réussie !"))
+    .catch(() => console.log("Connexion à MongoDB échouée !"));
+
+
+
+app.get('/lodgings', getAllLodgings = (req, res) => {
+    Lodging.find()
+        .then(lodgings => res.status(200).json(lodgings))
+        .catch(error => res.status(400).json({ error }))
+}
+)
 
 app.get('/', (req, res) => {
     res.send('Hey this is my API running 🥳')
