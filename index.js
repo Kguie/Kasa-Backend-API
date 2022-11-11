297c8858206f3f9ea95f4476804ab658204272ca
Kévin Guieba <kevin.guieba@gmail.com>
2022-11-10 22:53:14 -0400

    cors delete

--- index.js
+++ index.js
@@ -5,13 +5,7 @@ const app = express()
 const Lodging = require('./models/lodging')
 const PORT = 4000
 
-//Cors
-app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization");
-    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, PATCH, OPTIONS");
-    next();
-});
+
 
 
 //Database connection
