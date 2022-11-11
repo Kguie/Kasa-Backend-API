17774665caffc9b1dc15d059bf894ee550cde897
Kévin Guieba <kevin.guieba@gmail.com>
2022-11-10 22:33:45 -0400

    test4

--- index.js
+++ index.js
@@ -5,9 +5,14 @@ const app = express()
 const Lodging = require('./models/lodging')
 const PORT = 4000
 
-app.listen(PORT, () => {
-    console.log(`API listening on PORT ${PORT} `)
-})
+//Cors
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization");
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, PATCH, OPTIONS");
+    next();
+});
+
 
 //Database connection
 mongoose.connect("mongodb+srv://kguie:GfQLpqTpFOeXvAFr@kasa.nsgwlcc.mongodb.net/KasaDB", {
@@ -18,11 +23,10 @@ mongoose.connect("mongodb+srv://kguie:GfQLpqTpFOeXvAFr@kasa.nsgwlcc.mongodb.net/
     .catch(() => console.log("Connexion à MongoDB échouée !"));
 
 
-
-app.get('/lodgings', getAllLodgings = (req, res) => {
+app.use('/lodgings', getAllLodgings = async (req, res) => {
     Lodging.find()
-        .then(lodgings => res.status(200).json(lodgings))
-        .catch(error => res.status(400).json({ error }))
+        .then(lodgings => { res.send(res.status(200).json(lodgings)) })
+        .catch(error => { res.send(res.status(400).json({ error })) })
 }
 )
 
@@ -34,5 +38,8 @@ app.get('/about', (req, res) => {
     res.send('This is my about route..... ')
 })
 
+app.listen(PORT, () => {
+    console.log(`API listening on PORT ${PORT} `)
+})
 // Export the Express API
 module.exports = app
